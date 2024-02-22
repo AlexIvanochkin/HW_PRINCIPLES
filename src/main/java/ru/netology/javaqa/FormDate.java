8a14ffa25d21c8c0f17985b915c9798f0b88030c
Alex Neko <128819829+AlexIvanochkin@users.noreply.github.com>
2024-02-22 20:01:55 +0300

    Initial

---
+++ src/main/java/ru/netology/javaqa/FormDate.java
@@ -0,0 +1,10 @@
+package ru.netology.javaqa;
+
+public class FormDate {
+
+
+    public int day; // поле для дня рождения
+    public int month; // поле для месяца рождения
+    public int year; // поле для года рождения
+
+}
---
+++ src/main/java/ru/netology/javaqa/Post.java
@@ -0,0 +1,13 @@
+package ru.netology.javaqa;
+
+public class Post {
+
+    public String name; // поле для имени
+    public String passport; // поле для паспорта
+    public String patronymic; // поле для отчества
+    public String phone; //поле для номера телефона
+    public String surname; // поле для фамилии
+    public boolean subscription = true;
+    public FormDate birthday;
+
+}
